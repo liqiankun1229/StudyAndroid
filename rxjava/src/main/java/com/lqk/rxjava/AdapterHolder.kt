6804d2569dfaf70598d39b4e0aa60c1e9dc92668
李乾坤 <liqiankun1229@gmail.com>
2022-09-23 14:42:30 +0800

    2022/09/23 RxJava

---
+++ rxjava/src/main/java/com/lqk/rxjava/AdapterHolder.kt
@@ -0,0 +1,13 @@
+package com.lqk.rxjava
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+/**
+ * @author LQK
+ * @time 2022/3/15 16:33
+ * @remark
+ */
+class AdapterHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+}
