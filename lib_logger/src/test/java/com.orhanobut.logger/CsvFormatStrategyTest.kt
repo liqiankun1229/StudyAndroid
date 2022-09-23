ac83f4aafb97d2f97b118fbb7be1e7154eaf1515
李乾坤 <liqiankun1229@gmail.com>
2022-09-23 14:41:45 +0800

    2022/09/23 logger

---
+++ lib_logger/src/test/java/com.orhanobut.logger/CsvFormatStrategyTest.kt
@@ -0,0 +1,36 @@
+package com.orhanobut.logger
+
+import com.google.common.truth.Truth.assertThat
+import org.junit.Test
+
+class CsvFormatStrategyTest {
+
+  @Test fun log() {
+    val formatStrategy = CsvFormatStrategy.newBuilder()
+        .logStrategy { priority, tag, message ->
+          assertThat(tag).isEqualTo("PRETTY_LOGGER-tag")
+          assertThat(priority).isEqualTo(Logger.VERBOSE)
+          assertThat(message).contains("VERBOSE,PRETTY_LOGGER-tag,message")
+        }
+        .build()
+
+    formatStrategy.log(Logger.VERBOSE, "tag", "message")
+  }
+
+  @Test fun defaultTag() {
+    val formatStrategy = CsvFormatStrategy.newBuilder()
+        .logStrategy { priority, tag, message -> assertThat(tag).isEqualTo("PRETTY_LOGGER") }
+        .build()
+
+    formatStrategy.log(Logger.VERBOSE, null, "message")
+  }
+
+  @Test fun customTag() {
+    val formatStrategy = CsvFormatStrategy.newBuilder()
+        .tag("custom")
+        .logStrategy { priority, tag, message -> assertThat(tag).isEqualTo("custom") }
+        .build()
+
+    formatStrategy.log(Logger.VERBOSE, null, "message")
+  }
+}
